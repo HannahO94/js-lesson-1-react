66aa2804e683a14378398a16ccfd1fdff522e3bc
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-08-19 09:08:59 +0200

    övning

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import MyButton from "./components/MyButton";
+import Welcome from "./components/Welcome";
+import MyLink from "./components/MyLink";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
+         */}
+        <Welcome name="Hannah" />
         <p>
           JS lesson 1
           <MyButton title="Tryck här" />
           <MyButton title="Eller här" />
           <MyButton title="Varför inte här" />
         </p>
-
+        <MyLink title="www.google.com" name="Google" />
         <p>
           <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/MyLink.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyButton({ title, name }) {
+  return <a href={title}>{name}</a>;
+}
